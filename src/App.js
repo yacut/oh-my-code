ef8bd8a4eedcea25404fdaa8148d2733b4234443
yacut <yacut@protonmail.com>
2017-01-09 22:50:01 +0100

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
+import React, {Component} from 'react';
 import './App.css';
+import { Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
+import { MenuItem } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Oh My Code</h2>
-        </div>
-        <p className="App-intro">
-          This page helps you increase your productivity and write source code better.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+              <div className="App-header">
+                <Navbar inverse collapseOnSelect>
+                   <Navbar.Header>
+                     <Navbar.Brand>
+                        <a href="#">Oh My Code</a>
+                     </Navbar.Brand>
+                     <Navbar.Toggle />
+                   </Navbar.Header>
+                   <Navbar.Collapse>
+                     <Nav>
+                       <NavItem eventKey={1} href="#">Link</NavItem>
+                       <NavItem eventKey={2} href="#">Link</NavItem>
+                       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                         <MenuItem eventKey={3.1}>Action</MenuItem>
+                         <MenuItem eventKey={3.2}>Another action</MenuItem>
+                         <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                         <MenuItem divider />
+                         <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                       </NavDropdown>
+                     </Nav>
+                     <Nav pullRight>
+                       <NavItem eventKey={1} href="#">Link Right</NavItem>
+                       <NavItem eventKey={2} href="#">Link Right</NavItem>
+                     </Nav>
+                   </Navbar.Collapse>
+                 </Navbar>
+            </div>
+            <p className="App-intro">
+              This page helps you increase your productivity and write source code better.
+            </p>
+          </div>
+        );
+    }
 }
 
 export default App;
